8be7288ebe6638478af09a6e73cd233bb2907417
myname <benjamin.vandervleuten@gxsoftware.com>
2018-03-07 15:36:05 +0100

    ed

--- src/com/mycompany/jenkins/Release.groovy
+++ src/com/mycompany/jenkins/Release.groovy
@@ -181,7 +181,8 @@ class Release implements Serializable {
         } */
         steps.echo "Deploying jar"
 
-        steps.sh "curl -v -F r-${repository} -F g=${groupId} -F a=${artifactId} -F v=${releaseNumber} -F p=${packaging} -F c=sources -F e=jar file=@${folder}target/${artifactId}-${releaseNumber}-sources.jar -u admin:admin123"
+        steps.sh "curl -v -F r=${repository} -F hasPom=true -F e=jar -F file=@${folder}pom.xml -F file=@${folder}target/${artifactId}-${releaseNumber}.jar -u admin:admin123"
+        //steps.sh "curl -v -F r-${repository} -F g=${groupId} -F a=${artifactId} -F v=${releaseNumber} -F p=${packaging} -F c=sources -F e=jar file=@${folder}target/${artifactId}-${releaseNumber}-sources.jar -u admin:admin123"
 
     }
 
