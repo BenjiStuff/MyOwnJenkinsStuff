46f083ed1e7b7f606043cf599cd4634c81c2574e
myname <benjamin.vandervleuten@gxsoftware.com>
2018-03-07 15:56:07 +0100

    fix

--- src/com/mycompany/jenkins/Release.groovy
+++ src/com/mycompany/jenkins/Release.groovy
@@ -201,7 +201,7 @@ class Release implements Serializable {
             }
         } else {
             //steps.withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'nexus', passwordVariable: 'pwd', usernameVariable: 'un']]) {
-                steps.bat "curl -v -F r=${repository} -F hasPom=true -F file=@${folder}pom.xml -u ${env.un}:${env.pwd} http://localhost:8081/nexus/service/local/artifact/maven/content > target/deploy.log"
+                steps.bat "curl -v -F r=${repository} -F hasPom=true -F file=@${folder}pom.xml -u admin:admin123 http://localhost:8081/nexus/service/local/artifact/maven/content > target/deploy.log"
             //}
             verifyDeploy(folder)
         }
