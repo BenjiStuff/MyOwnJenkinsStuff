23dbf602e2a9d9c4193c2e12367e9196abf67fb7
myname <benjamin.vandervleuten@gxsoftware.com>
2018-03-07 15:45:16 +0100

    bat

--- src/com/mycompany/jenkins/Release.groovy
+++ src/com/mycompany/jenkins/Release.groovy
@@ -181,7 +181,7 @@ class Release implements Serializable {
         } */
         steps.echo "Deploying jar"
 
-        steps.sh "curl -v -F r=${repository} -F hasPom=true -F e=jar -F file=@${folder}pom.xml -F file=@${folder}target/${artifactId}-${releaseNumber}.jar -u admin:admin123 http://localhost:8081/nexus/service/local/artifact/maven/content > ${folder}target/deploy.log"
+        steps.bat "curl -v -F r=${repository} -F hasPom=true -F e=jar -F file=@${folder}pom.xml -F file=@${folder}target/${artifactId}-${releaseNumber}.jar -u admin:admin123 http://localhost:8081/nexus/service/local/artifact/maven/content > ${folder}target/deploy.log"
         //steps.sh "curl -v -F r-${repository} -F g=${groupId} -F a=${artifactId} -F v=${releaseNumber} -F p=${packaging} -F c=sources -F e=jar file=@${folder}target/${artifactId}-${releaseNumber}-sources.jar -u admin:admin123"
 
     }
@@ -213,7 +213,7 @@ class Release implements Serializable {
 
         steps.echo "Deploying Pom"
 
-        steps.sh "curl -v -F r=${repository} -F hasPom=true -F file=@${folder}pom.xml -u admin:admin123 http://localhost:8081/nexus/service/local/artifact/maven/content > target/deploy.log"
+        steps.bat "curl -v -F r=${repository} -F hasPom=true -F file=@${folder}pom.xml -u admin:admin123 http://localhost:8081/nexus/service/local/artifact/maven/content > target/deploy.log"
 
         for (module in modules) {
             steps.echo "processing module ${module}"
